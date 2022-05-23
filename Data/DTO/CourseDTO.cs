40b429111ae327c54683802a98f4be85bcdd2479
Varekai133 <stebleva.cs@gmail.com>
2022-05-23 03:49:51 +0300

    Add DataTable, Modal form, Navs. Fix db get function

--- Data/DTO/CourseDTO.cs
+++ Data/DTO/CourseDTO.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using DSRProject.Models;
 
 namespace DSRProject.Data.DTO;
 public class CourseDTO {
     public uint CourseId { get; set; }
     public float Value { get; set; }
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime Date { get; set;}
     public Currency Currency { get; set; }
 }
--- Servicies/CurrenciesRepository.cs
+++ Servicies/CurrenciesRepository.cs
@@ -17,14 +17,14 @@ public class CurrenciesRepository : ICurrenciesRepository {
     public List<DateTime> GetDates(string currencyId, DateTime firstDate, DateTime secondDate) {
         return _context.Courses
             .Where(c => c.Currency.CurrencyId == currencyId)
-            .Where(v => (v.Date > firstDate) && (v.Date < secondDate))
+            .Where(v => (v.Date >= firstDate) && (v.Date <= secondDate))
             .Select(e => e.Date)
             .OrderByDescending(o => o.Date).ToList();
     }
     public List<CourseDTO> GetCourses(string currencyId, DateTime firstDate, DateTime secondDate) {
         var coursesInDb = _context.Courses
             .Where(c => c.Currency.CurrencyId == currencyId)
-            .Where(v => (v.Date > firstDate) && (v.Date < secondDate))
+            .Where(v => (v.Date >= firstDate) && (v.Date <= secondDate))
             .OrderByDescending(o => o.Date).ToList();
         var listOfValutes = _context.Currencies
             .Where(c => c.CurrencyId == currencyId).ToList();
